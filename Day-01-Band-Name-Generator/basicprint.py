f9339130bfeb8047fd54b25e4670ffbf4a99032c
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-26 21:05:22 +0545

    Basic printing

---
+++ Day-01-Band-Name-Generator/basicprint.py
@@ -0,0 +1,15 @@
+print("Zen "+"Sama\n")
+
+print("Zen"+" "+"Sama\n")
+
+print("Hello\n"+"World\n") 
+
+print('print("What to print?")')
+
+print('Day 1 - String Manipulation')
+
+print('e.g: print("Hello"+"World")')
+
+print("String concatenation is done with the "+"+"+ " sign")
+
+print("New line can be created with a backslash and n.")
