6bfa7ec9c6f2e02ef5125a6c6d188a7279c1db1e
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-26 21:23:19 +0545

    swapping values of two variables

---
+++ Day-01-Band-Name-Generator/variables.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter your value for num1: "))
+num2 = int(input("Enter your value for num2: "))
+print()
+
+print("Value of num1 before swap: ",num1)
+print("Value of num2 before swap: ",num2)
+print()
+
+swap = num1
+num1 = num2
+num2 = swap
+
+print("Value of num1 after swap: ",num1)
+print("Value of num2 after swap: ",num2)
