f69b0da65d76f205f3f886666016c9701c5bc19f
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-27 11:43:51 +0545

    Days,Weeks,Months till 90

---
+++ Day-02-Tip-Calculator/days-week-month-till90.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age: "))
+
+remaining_days = (90 - age ) * 365
+remaining_weeks =(90 - age ) * 52
+remaining_months=(90 - age ) * 12
+
+print(f'You have {remaining_days} days,{remaining_weeks} weeks and {remaining_months} months')
