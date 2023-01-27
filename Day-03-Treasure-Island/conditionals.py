641eda041b72834e05c21801408292fa14f19d02
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-27 18:17:53 +0545

    basic conditional

---
+++ Day-03-Treasure-Island/conditionals.py
@@ -0,0 +1,7 @@
+print("Welcome to the RollerCoaster Ride!")
+height = int(input("Enter your height in cm: "))
+
+if height >= 120:
+    print("You can ride it. Just grab a ticket from the counter.")
+else:
+    print("Sorry you can't ride now.")
