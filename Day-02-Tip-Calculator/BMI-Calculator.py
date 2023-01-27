45e6eba7c66fc94315d575b36dc5a6264a95abea
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-27 09:16:39 +0545

    BMI Calculator = height(m) / weight(kg) ** 2

---
+++ Day-02-Tip-Calculator/BMI-Calculator.py
@@ -0,0 +1,6 @@
+height = float(input("Enter your height in metres: "))
+weight = float(input("Enter your weight in kgs: "))
+
+BMI = int(weight / (height ** 2))
+
+print("Your BMI in Integer value is ",BMI)
