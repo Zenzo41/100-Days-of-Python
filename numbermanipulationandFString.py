a607041e89a4f9ea252a706d0723c5afb86ff3a8
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-01-27 11:22:49 +0545

    Floor Division and Formatted String

---
+++ numbermanipulationandFString.py
@@ -0,0 +1,11 @@
+print(round(8/3,2))
+
+print(8 // 3) #floor division
+
+
+score = 1
+isWinning = True
+TimeRemaining = '30min'
+
+#Formatted String
+print(f'Your score is {score}. Is your team winning? {isWinning}. You have {TimeRemaining} left')
