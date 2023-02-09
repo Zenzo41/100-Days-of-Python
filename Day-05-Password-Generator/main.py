b78fb7ce13b31f9aff1dbc5e1ee2a27eb4bd7a83
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-02-09 19:20:08 +0545

    Hard way to generate password using random.shuffle

--- Day-05-Password-Generator/main.py
+++ Day-05-Password-Generator/main.py
@@ -11,25 +11,55 @@ upprcase = int(input("How many uppercase alphabet do you want in your password: 
 num = int(input("How many numbers do you want in your password: "))
 spcl_char = int(input("How many special characters do you want in your password: "))
 
-password = ''
+# Easy Way
+# password = ''
+# for char in range(1,lwrcase + 1):
+#     randomlwr = random.choice(lowercase_alphabet)
+#     password += randomlwr
+    
+# for char in range(1,spcl_char + 1):
+#     randomspcl = random.choice(special_characters)
+#     password += randomspcl
+    
+       
+# for char in range(1,upprcase + 1):
+#     randomuppr = random.choice(uppercase_alphabet)
+#     password += randomuppr
+    
+    
+# for char in range(1,num + 1):
+#     randomnum = random.choice(numbers)
+#     password += randomnum
+    
+# print(password)   
+
+#HARD LEVEL
+password_list = []
 for char in range(1,lwrcase + 1):
     randomlwr = random.choice(lowercase_alphabet)
-    password += randomlwr
+    password_list.append(randomlwr) 
     
 for char in range(1,spcl_char + 1):
     randomspcl = random.choice(special_characters)
-    password += randomspcl
+    password_list += randomspcl
     
        
 for char in range(1,upprcase + 1):
     randomuppr = random.choice(uppercase_alphabet)
-    password += randomuppr
+    password_list += randomuppr
     
     
 for char in range(1,num + 1):
     randomnum = random.choice(numbers)
-    password += randomnum
+    password_list += randomnum
     
-print(password)   
+print("Before Shuffle",password_list)
+random.shuffle(password_list)
+print("After Shuffle",password_list) 
 
-#HARD LEVEL
+password= ''
+for char in password_list:
+    password +=char
+    
+    
+print(f'Your password is: {password}')
