edf2c18c54cac93fda9681b8d4725c1ce25ae975
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-02-09 20:14:43 +0545

    Reeborg's World

--- Day-06-Functions/functions.py
+++ Day-06-Functions/functions.py
@@ -4,3 +4,32 @@ def my_function():
     
     
 my_function()
+
+# Reeborg's World : Moivng from 1 to 3 and again to 1
+
+# def turn_around():
+#     turn_left()
+#     turn_left()
+    
+    
+# move()
+# move()
+# turn_around()
+# move()
+# move()
+# turn_around() 
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# Moving in a square way
+# turn_left()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
+# turn_right()
+# move()
