2a6d052d96d3e6bafd5202848619be017e75102d
Sonam Syangbo <90586304+Zenzo41@users.noreply.github.com>
2023-04-27 20:26:37 +0545

    while loop added to continue till all filled

---
+++ Day-07-Beginner-Hangman/hangman3.py
@@ -0,0 +1,27 @@
+import random
+
+word_list = ["antelope", "capybara", "penguin"]
+chosen_word = random.choice(word_list)
+
+
+# list to append the blank space for total no of letter 
+display = []
+word_length = len(chosen_word)
+
+for _ in range(word_length):
+    display.append("_")
+    
+print("The word is :",display)
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+            
+    print(display)
+    
+    if "_" not in display:
+        end_of_game = True
